c05791ca91c98978ccab26ad9a611dc9e55b58a9
yZ1337 <32521997+yZ1337@users.noreply.github.com>
2024-05-16 13:14:34 +0200

    Update redirect.py

--- functions/redirect.py
+++ functions/redirect.py
@@ -7,8 +7,8 @@ def detect_open_redirect(url):
     try:
         response = requests.get(test_url)
         if response.status_code == 302 and "google.com" in response.headers.get('Location', ''):
-            print(f"{Fore.RED}[VULNERABLE] {Style.RESET_ALL}Open Redirect detected: {test_url}")
+            print(f"\n{Fore.RED}[VULNERABLE] {Style.RESET_ALL}Open Redirect detected: {test_url}")
         else:
-            print(f"{Fore.LIGHTGREEN_EX}[SAFE] {Style.RESET_ALL}No Open Redirect detected.")
+            print(f"\n{Fore.LIGHTGREEN_EX}[SAFE] {Style.RESET_ALL}No Open Redirect detected.")
     except requests.exceptions.RequestException as e:
-        print(f"{Fore.RED}[ERROR] {Style.RESET_ALL} Failed to test Open Redirect for {url}. Error: {e}")
+        print(f"\n{Fore.RED}[ERROR] {Style.RESET_ALL} Failed to test Open Redirect for {url}. Error: {e}")
