b16b4220b3df7688a8a8964a98f1d2cd7c4b46e4
yZ1337 <32521997+yZ1337@users.noreply.github.com>
2024-05-16 13:14:46 +0200

    Update cms.py

--- functions/cms.py
+++ functions/cms.py
@@ -5,12 +5,13 @@ def check_cms(url):
     try:
         response = requests.get(url)
         if "wp-content" in response.text or "wp-includes" in response.text:
-            print(f"{Fore.LIGHTGREEN_EX}WordPress CMS detected{Style.RESET_ALL}")
+            print(f"\n{Fore.LIGHTGREEN_EX}WordPress CMS detected{Style.RESET_ALL}")
+            print(f"\n{Fore.LIGHTBLUE_EX}[INFO] {Style.RESET_ALL}Use wpscan to find WordPress vulnerabilities.")
         elif "Joomla" in response.text:
-            print(f"{Fore.LIGHTGREEN_EX}Joomla CMS detected{Style.RESET_ALL}")
+            print(f"\n{Fore.LIGHTGREEN_EX}Joomla CMS detected{Style.RESET_ALL}")
         elif "Drupal" in response.text:
-            print(f"{Fore.LIGHTGREEN_EX}Drupal CMS detected{Style.RESET_ALL}")
+            print(f"\n{Fore.LIGHTGREEN_EX}Drupal CMS detected{Style.RESET_ALL}")
         else:
-            print(f"{Fore.YELLOW}No common CMS detected{Style.RESET_ALL}")
+            print(f"\n{Fore.YELLOW}No common CMS detected{Style.RESET_ALL}")
     except requests.exceptions.RequestException as e:
-        print(f"{Fore.RED}[ERROR] {Style.RESET_ALL} Failed to check CMS for {url}. Error: {e}")
+        print(f"\n{Fore.RED}[ERROR] {Style.RESET_ALL} Failed to check CMS for {url}. Error: {e}")
